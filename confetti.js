449857210b6525fab3d04dda437014af28a6e6a5
mathusummut <mathusum.mut@gmail.com>
2019-06-04 22:16:42 +0200

    Added fix recommended by Sejori

--- confetti.js
+++ confetti.js
@@ -1,18 +1,18 @@
 var confetti = {
-	maxCount: 150,      //set max confetti count
-	speed: 2,           //set the particle animation speed
-	frameInterval: 15,  //the confetti animation frame interval
-	alpha: 1.0,         //the alpha opacity of the confetti (between 0 and 1, where 1 is opaque and 0 is invisible)
-	gradient: false,    //whether to use gradients for the confetti particles
-	start: null,        //call to start confetti animation
-	stop: null,         //call to stop adding confetti
-	toggle: null,       //call to start or stop the confetti animation depending on whether it's already running
-	pause: null,        //call to freeze confetti animation
-	resume: null,       //call to unfreeze confetti animation
-	togglePause: null,  //call to toggle whether the confetti animation is paused
-	remove: null,       //call to stop the confetti animation and remove all confetti immediately
-	isPaused: null,     //call and returns true or false depending on whether the confetti animation is paused
-	isRunning: null     //call and returns true or false depending on whether the animation is running
+	maxCount: 150,		//set max confetti count
+	speed: 2,			//set the particle animation speed
+	frameInterval: 15,	//the confetti animation frame interval
+	alpha: 1.0,			//the alpha opacity of the confetti (between 0 and 1, where 1 is opaque and 0 is invisible)
+	gradient: false,	//whether to use gradients for the confetti particles
+	start: null,		//call to start confetti animation
+	stop: null,			//call to stop adding confetti
+	toggle: null,		//call to start or stop the confetti animation depending on whether it's already running
+	pause: null,		//call to freeze confetti animation
+	resume: null,		//call to unfreeze confetti animation
+	togglePause: null,	//call to toggle whether the confetti animation is paused
+	remove: null,		//call to stop the confetti animation and remove all confetti immediately
+	isPaused: null,		//call and returns true or false depending on whether the confetti animation is paused
+	isRunning: null		//call and returns true or false depending on whether the animation is running
 };
 
 (function() {
@@ -103,7 +103,7 @@ var confetti = {
 		if (canvas === null) {
 			canvas = document.createElement("canvas");
 			canvas.setAttribute("id", "confetti-canvas");
-			canvas.setAttribute("style", "display:block;z-index:999999;pointer-events:none");
+			canvas.setAttribute("style", "display:block;z-index:999999;pointer-events:none;position:absolute;top:0");
 			document.body.appendChild(canvas);
 			canvas.width = width;
 			canvas.height = height;
